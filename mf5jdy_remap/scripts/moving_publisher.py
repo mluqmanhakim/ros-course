d76c3ae9bcf83cd46241ae2e701460435f02b665
mluqmanhakim <mhd.luqmanhakim@gmail.com>
2022-10-16 09:34:41 +0200

    Edit publisher

--- mf5jdy_remap/scripts/moving_publisher.py
+++ mf5jdy_remap/scripts/moving_publisher.py
@@ -6,7 +6,6 @@ import click
 def moving_publisher():
     vel_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     moving_msg = Twist()
-    speed = 2
     t = 2
 
     while not rospy.is_shutdown():
@@ -14,7 +13,7 @@ def moving_publisher():
         click.echo()
 
         if cmd == "w":
-            moving_msg.linear.x = speed
+            moving_msg.linear.x = 2
             moving_msg.linear.y = 0
             moving_msg.linear.z = 0
             moving_msg.angular.x = 0
@@ -22,7 +21,7 @@ def moving_publisher():
             moving_msg.angular.z = 0
 
         elif cmd == "s":
-            moving_msg.linear.x = -speed
+            moving_msg.linear.x = -2
             moving_msg.linear.y = 0
             moving_msg.linear.z = 0
             moving_msg.angular.x = 0
@@ -30,7 +29,6 @@ def moving_publisher():
             moving_msg.angular.z = 0
 
         elif cmd == "a":
-            t = 1
             moving_msg.linear.x = 0
             moving_msg.linear.y = 0
             moving_msg.linear.z = 0
@@ -39,7 +37,6 @@ def moving_publisher():
             moving_msg.angular.z = 2
 
         elif cmd == "d":
-            t = 1
             moving_msg.linear.x = 0
             moving_msg.linear.y = 0
             moving_msg.linear.z = 0
@@ -57,16 +54,19 @@ def moving_publisher():
 
         t0 = rospy.Time.now().to_sec()
         cur_t = 0
-        while(cur_t <= t):
+        while(cur_t < t):
             vel_publisher.publish(moving_msg)
             t1 = rospy.Time.now().to_sec()
             cur_t = t1 - t0
 
         moving_msg.linear.x = 0
         moving_msg.linear.y = 0
+        moving_msg.linear.z = 0
+        moving_msg.angular.x = 0
+        moving_msg.angular.y = 0
+        moving_msg.angular.z = 0
         vel_publisher.publish(moving_msg)
         
-
 
 if __name__ == "__main__":
     rospy.init_node("rm_mf5jdy")
